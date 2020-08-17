b867218f329334a06273401a489d62e1cecb5e5e
cgrandjean <cristina.grandjean@soprasteria.com>
2020-08-17 14:49:06 +0200

    Reorganising the app So that the planner is one view but there also is a home page to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { DayBoxComponent } from './day-box/day-box.component';
-import { JournalCategoryComponent } from './journal-category/journal-category.component';
-import { TaskCategoryComponent } from './task-category/task-category.component';
-import { TaskInputComponent } from './task-category/task-input/task-input.component';
+import { HomeComponent } from './home/home.component';
+import { PlannerComponent } from './planner/planner.component';
+import { DayBoxComponent } from './planner/day-box/day-box.component';
+import { JournalCategoryComponent } from './planner/journal-category/journal-category.component';
+import { TaskCategoryComponent } from './planner/task-category/task-category.component';
+import { TaskInputComponent } from './planner/task-category/task-input/task-input.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, DayBoxComponent, JournalCategoryComponent, TaskCategoryComponent, TaskInputComponent ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule, 
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent},
+      { path: 'planner', component: PlannerComponent},
+      { path: '', redirectTo: 'home', pathMatch: 'full'},
+      { path: '**', redirectTo: 'home', pathMatch: 'full'}
+    ])
+  ],
+  declarations: [ 
+    AppComponent,
+    HomeComponent,
+    PlannerComponent, 
+    DayBoxComponent, 
+    JournalCategoryComponent, 
+    TaskCategoryComponent, 
+    TaskInputComponent 
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/planner/planner.component.ts
@@ -0,0 +1,67 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-planner',
+  templateUrl: './planner.component.html',
+  styleUrls: ['./planner.component.css']
+})
+export class PlannerComponent {
+
+  today: Date = new Date();
+  weekDays: string[] = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+
+  weekTasksCategories = [{
+    title: 'Goal of the week',
+    subtitle: 'Achieve it to go towards your dreams',
+    numberOfInputs: 1,
+    canAddTask: false},
+    {
+    title: 'Habits',
+    subtitle: 'Be the best version of yourself',
+    numberOfInputs: 3,
+    canAddTask: true},
+    {
+    title: 'General to dos',
+    subtitle: 'The things that need to be done',
+    numberOfInputs: 3,
+    canAddTask: true}
+  ];
+
+  dayTasksCategories = [{
+    title: 'Most important task',
+    subtitle: "If this was the only thing you did today you'd be satisfied",
+    numberOfInputs: 1,
+    canAddTask: false},
+    {
+    title: 'Habits',
+    subtitle: 'Consistency is the key to success',
+    numberOfInputs: 3,
+    canAddTask: true},
+    {
+    title: 'Professional tasks',
+    subtitle: 'Do these at work for a productive day',
+    numberOfInputs: 3,
+    canAddTask: true},
+    {
+    title: 'Personal tasks',
+    subtitle: "Don't forget yourself today",
+    numberOfInputs: 3,
+    canAddTask: true},
+    {
+    title: 'Events',
+    subtitle: 'Have people to see today ?',
+    numberOfInputs: 2,
+    canAddTask: true}
+    ];
+  
+  journalCategoriesDayTime = ['I am grateful for...', 'Daily affirmations'];
+
+  journalCategoriesNightTime= [{
+    title: '3 amazing things that happened today',
+    numberOfInputs: 3},
+    {
+    title: 'How could I have made today even better ?',
+    numberOfInputs: 2}
+    ];
+
+}
